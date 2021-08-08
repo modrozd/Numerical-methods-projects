28a44bfc0f4400421729e306be02b041f1ae1c30
Monika Drozd <40463186+modrozd@users.noreply.github.com>
2021-08-08 22:17:18 +0200

    Project 3: Height profile approximation
    The aim of the project is to implement selected interpolation approximation methods discussed in the lecture - it is a method using Lagrange interpolation polynomial and a method using third degree spline functions. 
    
    Celem projektu jest implementacja wybranych metod aproksymacji interpolacyjnej omawianych na wykładzie - jest to metoda wykorzystująca wielomian interpolacyjny Lagrange’a oraz metoda wykorzystująca funkcje sklejane trzeciego stopnia.

---
+++ project3_main.py
@@ -0,0 +1,8 @@
+# Projekt nr 3 Metody Numeryczne
+# Aproksymacja profilu wysokościowego
+
+from Spline import interpolate_using_spline
+from Lagrange import interpolate_using_lagrange
+
+# interpolate_using_spline(8) #55 20 8
+interpolate_using_lagrange(55)
